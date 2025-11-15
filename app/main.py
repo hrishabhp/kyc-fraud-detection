dfdcac3a34f171b8ecd4f75cc798700577acb9b1
hrishabhp <hrishabh.pal@gmail.com>
2025-11-15 22:54:30 +0530

    Added PAN validation API (regex bug fixed + uppercase handling)

--- app/main.py
+++ app/main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "API is working"}
+
+import re
+from fastapi import FastAPI
+
+app= FastAPI()
+@app.get("/validate-pan")
+def validate_pan(pan: str):
+    pan = pan.upper() #Fix: Handle lowercase PAN inputs
+    pattern = r"^[A-Z]{5}[0-9]{4}[A-Z]$"
+
+    if re.match(pattern, pan):
+        return {"pan": pan, "valid": True}
+    else:
+        return {"pan": pan, "valid": False}
+
+
